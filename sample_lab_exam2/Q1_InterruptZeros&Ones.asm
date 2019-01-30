8063b95a1d9df4c8fd05fa842e12fb12986f796f
NiallS4 <44320530+NiallS4@users.noreply.github.com>
2019-01-30 19:57:04 +0000

    Add files via upload

--- sample_lab_exam2/Q1_InterruptZeros&Ones.asm
+++ sample_lab_exam2/Q1_InterruptZeros&Ones.asm
@@ -5,36 +5,36 @@
 ; does nothing. You will also need a Timer ISR.
 	
 	JMP initialise
-	DB 50					; Interrupt at address 50
+	DB 50				; Interrupt at address 50
 
 initialise:
-	STI						; Enable hardware interrupts
-	MOV BL, C0				; Set BL to start at position C0
-	MOV CL, C1				; Set CL to start at position C1
+	STI				; Enable hardware interrupts
+	MOV BL, C0			; Set BL to start at position C0
+	MOV CL, C1			; Set CL to start at position C1
 
 mainCode:
 	JMP mainCode			; Infinite loop
 
 	ORG 50
-	PUSHF					; Save flags
-	CMP CL, 00				; Check if CL < 00
-	JNS end					; If false end program
-	CMP AL, 30				; Check if last displayed character on VDU is '0'
-	JZ one					; If true jump to 'one' loop
-	JS zero					; Else jump to 'zero' loop
+	PUSHF				; Save flags
+	CMP CL, 00			; Check if CL < 00
+	JNS end				; If false end program
+	CMP AL, 30			; Check if last displayed character on VDU is '0'
+	JZ one				; If true jump to 'one' loop
+	JS zero				; Else jump to 'zero' loop
 
 zero:
-	MOV AL, 30				; Move ASCII code for 0 into AL
+	MOV AL, 30			; Move ASCII code for 0 into AL
 	MOV [BL], AL			; Display on VDU
-	ADD BL, 02				; Increment BL by 2
-	POPF					; Restore flags
+	ADD BL, 02			; Increment BL by 2
+	POPF				; Restore flags
 	IRET
 
 one:
-	MOV AL, 31				; Move ASCII code for 1 into AL
+	MOV AL, 31			; Move ASCII code for 1 into AL
 	MOV [CL], Al			; Display on VDU
-	ADD CL, 02				; Increment CL by 2
-	POPF					; Restore flags
+	ADD CL, 02			; Increment CL by 2
+	POPF				; Restore flags
 	IRET
 
 end:
--- sample_lab_exam2/Q2-SumOddNumbers.asm
+++ sample_lab_exam2/Q2-SumOddNumbers.asm
@@ -8,7 +8,7 @@
 ; be useful. Also, it may or may not be helpful to remember: multiplying by 1 preserves a number.
 ; multiplying by 0 gives 0.
 
-	ORG A0					; Test numbers at memory location A0
+	ORG A0				; Test numbers at memory location A0
 	DB 01
 	DB 02
 	DB 03
@@ -21,35 +21,35 @@
 
 	ORG 00
 
-	MOV AL, A0				; Start checking numbers from location A0
-	MOV BL, A9				; Stop at location A9
+	MOV AL, A0			; Start checking numbers from location A0
+	MOV BL, A9			; Stop at location A9
 
 loop:
-	CALL 50					; Call procedure at memory location 50
-	CMP AL, BL				; Check if AL == BL (i.e. A9 == A9)
-	JZ end					; If true end program
+	CALL 50				; Call procedure at memory location 50
+	CMP AL, BL			; Check if AL == BL (i.e. A9 == A9)
+	JZ end				; If true end program
 	INC AL
-	JMP loop				; Else move to next memory location
+	JMP loop			; Else move to next memory location
 	HALT
 
 	ORG 50
-	PUSHF					; Save flags
-	PUSH BL					; Backup BL to stack
+	PUSHF				; Save flags
+	PUSH BL				; Backup BL to stack
 	MOV BL, [AL]			; Move number in AL to BL
-	MOD BL, 02				; Divide BL by 2
-	CMP BL, 00				; Check if BL is now 0 (i.e. number is even)
-	POP BL					; Restore BL to original number
-	JZ exit					; If BL is 0 exit procedure
+	MOD BL, 02			; Divide BL by 2
+	CMP BL, 00			; Check if BL is now 0 (i.e. number is even)
+	POP BL				; Restore BL to original number
+	JZ exit				; If BL is 0 exit procedure
 	JMP continue			; Else continue
 
 continue:
-	PUSH BL					; Backup BL to stack
+	PUSH BL				; Backup BL to stack
 	MOV BL, [AL]			; Move number in AL to BL
-	ADD CL, BL				; Store sum of odd numbers in CL
-	POP BL					; Restore BL to original number
+	ADD CL, BL			; Store sum of odd numbers in CL
+	POP BL				; Restore BL to original number
 
 exit:
-	POPF					; Restore flags
+	POPF				; Restore flags
 	RET
 
 end:
